2ebaf6b604fe58703eb4884452e1727f97180139
alirezangh <alireza20002485@gmail.com>
2023-12-28 04:47:45 -0800

    finish blog-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -68,5 +68,10 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({addVariant}){
+      addVariant(`child`,`&>*`);
+      addVariant(`child-hover`,`&>*:hover`)
+    }
+  ],
 };
